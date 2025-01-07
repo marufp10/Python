8552d87b47ff1243423f90e39eff3a0a6c5da10a
Maruf Parvez <marufp10@gmail.com>
2025-01-06 22:13:42 -0500

    My Praxctice

---
+++ my_python/Practice.py
@@ -0,0 +1,23 @@
+# This is a practice inbuilt function in python
+
+#Concatenation of two strings
+First_name = "Maruf"
+Last_name = "Parvez"
+
+result = First_name + " " + Last_name
+
+print (result)
+
+#Split of a string
+
+str1 = "User Id: 2215: /n User Name: Maruf Parvez"
+print (str1.split("/n")[1])
+
+#Replace a string
+str2 = "I am a student"
+print (str2.replace("student", "teacher"))
+
+#Upper case and Lower case
+str3 = "I am a maruf"
+print (str3.upper())
+print (str3.lower())
---
+++ my_python/Variables.py
@@ -0,0 +1,15 @@
+integer_variable = 5
+string_variable = 'John'
+
+assert integer_variable == 5
+assert string_variable == 'John'
+
+if integer_variable !=0:
+    print('false')
+
+
+
+variable_with_changed_type = 4
+variable_with_changed_type = 'Sally'
+
+assert variable_with_changed_type == 'Sally'
